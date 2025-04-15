a37533394f1f447dd68249b367f638d164fc6204
iamkingclutch3 <diegolopezgundin@gmail.com>
2025-04-16 01:30:10 +0200

    Edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { startServer } from "./src/server/api.js";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -17,5 +16,3 @@ export default defineConfig({
     outDir: "dist",
   },
 });
-
-startServer();
